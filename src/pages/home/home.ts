083c6528e25836549ff2766c542dde99cb108d5c
Alexander Peng <alexanderjpeng@gmail.com>
2018-12-06 17:42:47 -0800

    FINAL

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,11 +10,22 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: 'home.html'
 })
 export class HomePage {
+	announcerArray: any;
+	announcerFiltered: any;
 	announcer: Observable<any[]>;
 	selectedList: any;
 	constructor(public navCtrl: NavController, public firebaseProvider: FirebaseStoreProvider, public http: HttpClient) {
 		this.announcer = firebaseProvider.listAnnouncer();
 		this.selectedList = [];
+		this.announcerArray = [];
+		this.announcer.subscribe(item => {
+			this.announcerArray = [];
+			item.map(c => {
+				this.announcerArray.push(c);
+			})
+		this.announcerFiltered = this.announcerArray;
+		});
+
 	}
 
 	selectClass(selected) {
@@ -27,6 +38,21 @@ export class HomePage {
 			this.selectedList.splice(index, 1);
 		}
 		console.log(this.selectedList);
+	}
+
+	filterClass(ev: any) {
+		this.announcerFiltered = this.announcerArray;
+		const val = ev.target.value;
+		if(val && val.trim() != '') {
+			this.announcerFiltered = this.announcerFiltered.filter((item) => {
+				console.log(item);
+				return(
+					(item["Course Name"] && item["Course Name"].toLowerCase().indexOf(val.toLowerCase()) > -1) ||
+					(item["Teacher"] && item["Teacher"].toLowerCase().indexOf(val.toLowerCase()) > -1) || 
+					(item["Block"] && item["Block"].toLowerCase().indexOf(val.toLowerCase()) > -1)
+					);
+			});
+		}
 	}
 
 	nextPage() {
